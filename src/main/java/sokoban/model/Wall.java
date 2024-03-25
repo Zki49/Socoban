c13629b95f21dd4e5f89ae079b22e71d0094cf3e
Diallo <diallo.ismail2016@gmail.com>
2024-03-25 16:07:40 +0100

    Perf:(debug)

--- src/main/java/sokoban/model/Wall.java
+++ src/main/java/sokoban/model/Wall.java
@@ -1,12 +1,7 @@
 package sokoban.model;
 
 class Wall extends ObjectInMap{
-
-<<<<<<< HEAD
-     Wall() {
-=======
     Wall() {
->>>>>>> b0f4f23 (Perf:(Model))
         setTypeOfObjectInMap(TypeOfObjectInMap.WALL);
     }
 }
