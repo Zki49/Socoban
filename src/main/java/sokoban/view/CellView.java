664c6a1b46d1f6da2037e71436f839dc91c53234
Zakaria Guendouzi <guendouzi2003@gmail.com>
2024-03-25 14:12:46 +0100

    feat(iamge) : ajout images static

--- src/main/java/sokoban/view/CellView.java
+++ src/main/java/sokoban/view/CellView.java
@@ -1,7 +1,12 @@
 package sokoban.view;
 
+import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
 
 public abstract class CellView extends StackPane {
-
+    static Image ground = new Image("ground.png");
+    static Image box = new Image("box.png");
+    static Image goal = new Image("goal.png");
+    static Image player = new Image("player.png");
+    static Image wall = new Image("wall.png");
 }
