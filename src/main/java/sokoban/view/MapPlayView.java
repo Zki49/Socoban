09f86050f0ae237a73db7ed98129a70460b3b6ed
djiby sow <dsow1040@gmail.com>
2024-04-09 17:17:12 +0200

    feat(): size of map

--- src/main/java/sokoban/view/MapPlayView.java
+++ src/main/java/sokoban/view/MapPlayView.java
@@ -45,15 +45,29 @@ public class MapPlayView extends GridPane {
                 widthProperty(),
                 heightProperty(),
                 headerBox.heightProperty());*/
+        DoubleBinding cellSize3 = Bindings.createDoubleBinding( () -> {
+                    return calculateCellSize(mapWidth.get(), mapHeight.get(), MAP_WIDTH, MAP_HEIGHT);
+                }
+
+                , mapWidth, mapHeight);
+
+
         DoubleBinding cellHeight = mapHeight
                 .subtract(PADDING * 2)
                 .divide(MAP_HEIGHT);
         for (int i = 0; i < MAP_HEIGHT; ++i) {
             for (int j = 0; j < MAP_WIDTH; ++j) {
-                CellPlayView cellPlayView = new CellPlayView(mapPlayViewModel.getCellViewModel(i, j), cellSize2);
+                CellPlayView cellPlayView = new CellPlayView(mapPlayViewModel.getCellViewModel(i, j), cellSize3);
                 add(cellPlayView, j, i); // lignes/colonnes inversées dans gridpane
             }
         }
 
+    }
+    public double calculateCellSize(double availableWidth, double availableHeight, int numCellsWidth, int numCellsHeight) {
+        double cellSizeWidth = availableWidth / numCellsWidth;
+        double cellSizeHeight = availableHeight / numCellsHeight;
+
+        // Choose the smaller of the two cell sizes to ensure the cells fit within the available space
+        return Math.min(cellSizeWidth, cellSizeHeight);
     }
 }
